f541bd0f4985be898b8efe9cfc2d488b03d2022f
otauk <otauk@users.noreply.github.com>
2014-12-04 08:46:53 +0100

    sec reasons

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$con = mysqli_connect('localhost','d01c63c9','AFnPqDrvAFzMPYX7','d01c63c9');
-if (!$con) {
-  die('Could not connect: ' . mysqli_error($con));
-}
-// utf-8 erzwingen wegen umlauten
-$utf = "SET NAMES 'utf8'";
-$utf_query=$con->query($utf);
-?>
